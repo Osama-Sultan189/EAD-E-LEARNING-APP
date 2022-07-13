5a9c628644ab91a0ad7f11dc38282ab8c4ba2ca6
Osama Sultan <59367747+Osama-Sultan189@users.noreply.github.com>
2022-07-14 00:52:11 +0500

    added views and controllers

---
+++ E-learning-App/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_learning_App.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Signin()
+        {
+            return View();
+        }
+        public IActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
