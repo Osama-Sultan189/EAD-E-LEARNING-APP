5a5a3c2bdb7a0330dfb4e573c126a583a972b44e
Osama Sultan <59367747+Osama-Sultan189@users.noreply.github.com>
2022-07-15 17:53:02 +0500

    course controller and view created

---
+++ E-learning-App/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_learning_App.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
