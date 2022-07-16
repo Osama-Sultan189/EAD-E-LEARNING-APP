318940eb10993c70debd13475116b63f51d02657
Osama Sultan <59367747+Osama-Sultan189@users.noreply.github.com>
2022-07-16 13:45:03 +0500

    Student Crud operations

--- E-learning-App/Models/StudentRepository.cs
+++ E-learning-App/Models/StudentRepository.cs
@@ -8,12 +8,12 @@ namespace E_learning_App.Models
         string conn = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public void addStudent(Student s)
         {
-            //SqlConnection con = new SqlConnection(conn);
-            //con.Open();
-            //string query = "insert into [Student] values('" + s.Id + "','" + s.Name + "','" + s.Email + "','" + s.Password + "','" + s.Phone + "','" + s.Address + "','" + s.City + "','" + s.State + "','" + s.Zip + "','" + s.Country + "','" + s.Image + "','" + s.Role + "','" + s.Status + "','" + s.CreatedAt + "','" + s.UpdatedAt + "')";
-            //SqlCommand cmd = new SqlCommand(query, con);
-            //cmd.ExecuteNonQuery();
-            //con.Close();
+            SqlConnection con = new SqlConnection(conn);
+            con.Open();
+            string query = "insert into [Student] values('" + s.Id + "','" + s.Name + "','" + s.Email + "','" + s.Password+")";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
         public void removeStudent(int id)
         {
